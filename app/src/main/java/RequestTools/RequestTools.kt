70a148b4fc2ba7f399b54a779b44f6e971809774
gingledoof <62526960+gingledoof@users.noreply.github.com>
2020-06-29 22:36:15 -0400

    Update RequestTools.kt

--- app/src/main/java/RequestTools/RequestTools.kt
+++ app/src/main/java/RequestTools/RequestTools.kt
@@ -1,5 +1,6 @@
 package RequestTools
 
+import android.app.Activity
 import com.e.myapplication.PostActivity
 import com.e.myapplication.R
 
@@ -19,8 +20,6 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.ImageLoader
-import com.android.volley.toolbox.ImageRequest
-import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
 import com.e.myapplication.MainActivity
 import org.json.JSONArray
@@ -34,6 +33,54 @@ val TAG = "Request Tools"
 
 //Currently only sets thumbnail url on passed jsonarray
 
+class Post(jsonObject: JSONObject) {
+    val source: String
+    val directory: String
+    val hash: String
+    val height: Int
+    val id: Int
+    val image: String
+    val change: Int
+    val owner: String
+    val rating: String
+    val score: String
+    val tags: List<String>
+    val width: Int
+    val file_url: String
+    val created_at: String
+    lateinit var view: View
+    lateinit var request: Any
+    lateinit var reqManager: SingletonManager
+    var init_post = false
+    var loaded = false
+    lateinit var thumbnail_url: String
 
+    init {
+        this.source = jsonObject["source"] as String
+        this.directory = jsonObject["directory"] as String
+        this.hash = jsonObject["hash"] as String
+        this.height = jsonObject["height"] as Int
+        this.id = jsonObject["id"] as Int
+        this.image = jsonObject["image"] as String
+        this.change =  jsonObject["change"] as Int
+        this.owner = jsonObject["owner"] as String
+        this.rating = jsonObject["rating"] as String
+        this.score = jsonObject["score"] as String
+        this.tags = (jsonObject["tags"] as String).split(" ")
+        this.width = jsonObject["width"] as Int
+        this.file_url = jsonObject["file_url"] as String
+        this.created_at = jsonObject["created_at"] as String
+        getThumbnailUrl(jsonObject)
+    }
 
+    fun getThumbnailUrl(json: JSONObject){
+        val file = json["image"] as String
+        val thumbnail = json["file_url"].toString()
+            .replace("images", "thumbnails")
+            .replace(file, "thumbnail_" + file)
+            .replace("img2", "img1")
+            .replaceAfterLast(".", "jpg")
+        this.thumbnail_url = thumbnail
+    }
+}
 
